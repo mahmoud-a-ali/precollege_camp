318cd918447fa7e639b6f5673d4adf17b2a99edb
Mahmoud Ali <mahmud.aly.ahmad@gmail.com>
2024-07-23 15:18:55 -0400

    Update create_turtlebot_sim.sh

--- create_turtlebot_sim.sh
+++ create_turtlebot_sim.sh
@@ -3,15 +3,15 @@
 source /opt/ros/humble/setup.bash
 export TURTLEBOT3_MODEL=burger
 
-mkdir -p ~/turtlebot_sim/src
-cd ~/turtlebot_sim/src
+mkdir -p ~/turtlebot_sim2/src
+cd ~/turtlebot_sim2/src
 
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git -b humble-devel
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git -b humble-devel
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git -b humble-devel
 git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git -b humble-devel
 
-cd ~/turtlebot_sim
+cd ~/turtlebot_sim2
 colcon build 
 
 source install/setup.bash
