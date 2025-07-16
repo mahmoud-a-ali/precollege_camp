bc91b8673ae9fc38650bd88f0ecd37ab25b830f5
Mahmoud Ali <mahmud.aly.ahmad@gmail.com>
2025-07-16 17:00:38 -0400

    Update create_turtlebot_sim.sh
    update branch name

--- create_turtlebot_sim.sh
+++ create_turtlebot_sim.sh
@@ -6,7 +6,7 @@ export TURTLEBOT3_MODEL=burger
 mkdir -p ~/turtlebot_sim2/src
 cd ~/turtlebot_sim2/src
 
-git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git -b humble-devel
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git -b humble
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git -b humble-devel
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git -b humble-devel
 git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git -b humble-devel
